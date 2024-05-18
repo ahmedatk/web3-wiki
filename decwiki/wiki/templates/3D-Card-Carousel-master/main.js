dbf7eec399e67898973cebe5699981223cb2d1eb
Satyam <139366115+SatyamPote@users.noreply.github.com>
2024-05-19 03:34:46 +0530

    Update main.js

--- decwiki/wiki/templates/3D-Card-Carousel-master/main.js
+++ decwiki/wiki/templates/3D-Card-Carousel-master/main.js
@@ -12,3 +12,24 @@
         return rotate();
     });
 }).call(this);
+
+
+
+
+
+
+
+$(document).on('change', '.file-input', function() {
+        
+
+  var filesCount = $(this)[0].files.length;
+  
+  var textbox = $(this).prev();
+
+  if (filesCount === 1) {
+    var fileName = $(this).val().split('\\').pop();
+    textbox.text(fileName);
+  } else {
+    textbox.text(filesCount + ' files selected');
+  }
+});
